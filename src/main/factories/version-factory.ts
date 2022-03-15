36377b70f4989440d737ea0fea49b551511e3a03
Diandria Xavier <diandriaxavier@gmail.com>
2022-03-15 00:00:26 -0300

    feat: create version factory

---
+++ src/main/factories/version-factory.ts
@@ -0,0 +1,8 @@
+import { VersionController } from '../../controllers/implementations'
+import { VersionUseCase } from '../../usecases/implementations'
+
+export const makeVersionController = (): VersionController => {
+  const versionUseCase = new VersionUseCase()
+
+  return new VersionController(versionUseCase)
+}
