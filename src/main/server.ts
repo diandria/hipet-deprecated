2cf3bed2b73720f91f26a6a3f0933c5b4896abe1
Diandria Xavier <diandriaxavier@gmail.com>
2022-04-14 01:14:43 -0300

    feat: add port to local

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,6 @@ import env from './env'
 MongoHelper.connect(env.mongodb.mongoUrl)
   .then(async () => {
     const app = (await import('../../config/web/config/app')).default
-    app.listen(process.env.PORT, () => console.log(`Server running at http://localhost:${process.env.PORT}`))
+    app.listen((process.env.PORT || env.app.port), () => console.log(`Server running at http://localhost:${(process.env.PORT || env.app.port)}`))
   })
   .catch(console.error)
