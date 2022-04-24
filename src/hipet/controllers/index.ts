182204f3e969220163d6f62b5b55bf250f6b1965
Diandria Xavier <diandriaxavier@gmail.com>
2022-04-24 01:31:14 -0300

    feat: create CreatePost controller

--- src/hipet/controllers/index.ts
+++ src/hipet/controllers/index.ts
@@ -3,3 +3,5 @@ export * from './apresentation/version-controller'
 export * from './user/create-user-controller'
 export * from './user/login-user-controllers'
 export * from './user/get-user-controller'
+
+export * from './post/create-post-controller'
