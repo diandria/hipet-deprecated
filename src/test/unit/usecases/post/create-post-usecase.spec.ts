c2dc6f9f12b1c715d8b5a72489b4ee1b11b278d7
Diandria Xavier <diandriaxavier@gmail.com>
2022-04-28 00:57:00 -0300

    test: should return USER_NOT_EXISTS status if get user returns null

--- src/test/unit/usecases/post/create-post-usecase.spec.ts
+++ src/test/unit/usecases/post/create-post-usecase.spec.ts
@@ -1,30 +1,45 @@
 import { PostRepository } from '../../../../hipet/repositories/interfaces'
 import { CreatePostUseCase } from '../../../../hipet/usecases/implementations'
 import { CreatePostResultStatusOptions, CreatePostUseCaseInterface } from '../../../../hipet/usecases/interfaces'
-import { PostRepositoryStub } from '../../../mocks/repositories'
+import { mockAnimal } from '../../../mocks/entity'
+import { PostRepositoryStub, UserRepositoryStub } from '../../../mocks/repositories'
 
 interface SutTypes {
   sut: CreatePostUseCaseInterface
   postRepositoryStub: PostRepository
+  userRepositoryStub: UserRepositoryStub
 }
 
 const makeSut = (): SutTypes => {
   const postRepositoryStub = new PostRepositoryStub()
-  const sut = new CreatePostUseCase({ postRepository: postRepositoryStub })
+  const userRepositoryStub = new UserRepositoryStub()
+  const sut = new CreatePostUseCase({ postRepository: postRepositoryStub, userRepository: userRepositoryStub })
   return {
     sut,
-    postRepositoryStub
+    postRepositoryStub,
+    userRepositoryStub
   }
 }
 
 const makePostRequest = {
-  title: 'any_title',
-  text: 'any_post_text',
-  userNickname: 'any_nickname'
+  userEmail: 'any_email@mail.com',
+  picture: 'any_url.com.br',
+  description: 'any_description',
+  animal: mockAnimal()
 }
 
 describe('Post - Use Case', () => {
   describe('Create Post', () => {
+    test('Should return USER_NOT_EXISTS status if get user returns null', async () => {
+      const { sut, userRepositoryStub } = makeSut()
+      jest.spyOn(userRepositoryStub, 'findUserBy').mockImplementationOnce(async () => null) // throw
+
+      const createPostResult = await sut.create(makePostRequest)
+      expect(createPostResult).toEqual({
+        status: CreatePostResultStatusOptions.user_not_found
+      })
+    })
+
     test('Should return REPOSITORY_ERROR status if add method from repository throws', async () => {
       const { sut, postRepositoryStub } = makeSut()
       jest.spyOn(postRepositoryStub, 'add').mockImplementationOnce(async () => false) // throw
