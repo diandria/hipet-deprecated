b2a7b52d4e266e1f9f2e5cf0e5ea701aaacd1276
Diandria Xavier <diandriaxavier@gmail.com>
2022-04-28 01:05:28 -0300

    refactor: add user repository in controller factory

--- src/main/factories/post-factory.ts
+++ src/main/factories/post-factory.ts
@@ -1,10 +1,11 @@
 import { CreatePostController } from '../../hipet/controllers'
-import { MongoPostRepository } from '../../hipet/repositories/implementations'
+import { MongoPostRepository, MongoUserRepository } from '../../hipet/repositories/implementations'
 import { CreatePostUseCase } from '../../hipet/usecases/implementations'
 
 export const makeCreatePostController = (): CreatePostController => {
   const postRepository = new MongoPostRepository()
-  const postUseCases = new CreatePostUseCase({ postRepository })
+  const userRepository = new MongoUserRepository()
+  const postUseCases = new CreatePostUseCase({ postRepository, userRepository })
 
   return new CreatePostController(postUseCases)
 }
