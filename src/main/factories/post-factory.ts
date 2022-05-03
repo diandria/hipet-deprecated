184ea31140b4bcf01376513b864e7d4e1c8816b3
Diandria Xavier <diandriaxavier@gmail.com>
2022-05-02 23:32:34 -0300

    feat: create list post factory

--- src/main/factories/post-factory.ts
+++ src/main/factories/post-factory.ts
@@ -1,6 +1,6 @@
-import { CreatePostController } from '../../hipet/controllers'
+import { CreatePostController, ListPostController } from '../../hipet/controllers'
 import { MongoPostRepository, MongoUserRepository } from '../../hipet/repositories/implementations'
-import { CreatePostUseCase } from '../../hipet/usecases/implementations'
+import { CreatePostUseCase, ListPostUseCase } from '../../hipet/usecases/implementations'
 
 export const makeCreatePostController = (): CreatePostController => {
   const postRepository = new MongoPostRepository()
@@ -8,4 +8,11 @@ export const makeCreatePostController = (): CreatePostController => {
   const postUseCases = new CreatePostUseCase({ postRepository, userRepository })
 
   return new CreatePostController(postUseCases)
+}
+
+export const makeListPostController = (): ListPostController => {
+  const postRepository = new MongoPostRepository()
+  const postUseCases = new ListPostUseCase({ postRepository })
+
+  return new ListPostController(postUseCases)
 }
