ac6b2c72b3c504e7481757c64dc7f5271b95d02f
Diandria Xavier <diandriaxavier@gmail.com>
2022-05-02 23:32:56 -0300

    feat: create GET /post/list route

--- src/main/web/routes/post-routes.ts
+++ src/main/web/routes/post-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { makeCreatePostController } from '../../factories'
+import { makeCreatePostController, makeListPostController } from '../../factories'
 import { adaptRoute } from '../../../../config/web/adapters/express-route-adapter'
 
 export default (router: Router): void => {
   router.post('/post/create', adaptRoute(makeCreatePostController()))
+  router.get('/post/list', adaptRoute(makeListPostController()))
 }
