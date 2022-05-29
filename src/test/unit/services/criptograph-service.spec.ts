5b18391b328b6d941970dfd8c7ad8adb4e0db04c
Diandria Xavier <diandriaxavier@gmail.com>
2022-05-29 20:57:41 -0300

    test: create unit test to Cryptograph Service

---
+++ src/test/unit/services/criptograph-service.spec.ts
@@ -0,0 +1,31 @@
+import { NodeCriptographService } from '../../../hipet/services/implementations'
+import { CriptographService } from '../../../hipet/services/interfaces'
+
+describe('Criptograph Service', () => {
+  const makeSut = (): CriptographService => {
+    return new NodeCriptographService()
+  }
+
+  const decodedValueMock = 'any_decoded_value'
+  const encodedValueMock = 'YW55X2RlY29kZWRfdmFsdWU='
+
+  describe('Encrypt method', () => {
+    test('Shoult return an encrypt string', async () => {
+      const sut = makeSut()
+      const encryptValue = await sut.encrypt(decodedValueMock)
+
+      expect(typeof encryptValue).toBe('string')
+      expect(encryptValue).toBe(encodedValueMock)
+    })
+  })
+
+  describe('Decrypt method', () => {
+    test('Shoult return an decrypt string', async () => {
+      const sut = makeSut()
+      const decryptValue = await sut.decrypt(encodedValueMock)
+
+      expect(typeof decryptValue).toBe('string')
+      expect(decryptValue).toBe(decodedValueMock)
+    })
+  })
+})
