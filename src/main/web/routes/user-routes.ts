74c86287db0c7f3eb63fa1a542f01f5b565eeb42
Diandria Xavier <diandriaxavier@gmail.com>
2023-03-26 22:30:20 -0300

    feat: add endpoint to find user by nickname

--- src/main/web/routes/user-routes.ts
+++ src/main/web/routes/user-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { makeCreateUserController, makeFindUserByIdController } from '../../factories'
+import { makeCreateUserController, makeFindUserByIdController, makeFindUserByNicknameController } from '../../factories'
 import { adaptRoute } from '../../../../config/web/adapters/express-route-adapter'
 
 export default (router: Router): void => {
   router.post('/user/create', adaptRoute(makeCreateUserController()))
   router.get('/user/find-by-id/:id', adaptRoute(makeFindUserByIdController()))
+  router.get('/user/find-by-nickname/:nickname', adaptRoute(makeFindUserByNicknameController()))
 }
